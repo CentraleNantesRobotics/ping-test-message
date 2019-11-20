8fcbd3310925e7ffba282bc65d183b99706971a9
Henrique Martinez <rocamora.henrique@gmail.com>
2019-11-20 15:34:55 +0100

    erased 460 device related files and it can communicate with the ping360

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "ping-port-linux.h"
-#include <ping-device-ping1d.h>
+#include <ping-device.h>
 #include "ping-message-common.h"
-#include "ping-message-ping1d.h"
 #include "ping-port-linux.h"
 #include <iostream>
 
@@ -9,16 +8,18 @@
 int main(){
     const char* port = "/dev/ttyUSB0";
     PingPortLinux sonarPort = PingPortLinux(port);
-    Ping1d sonar = Ping1d(sonarPort);
+    PingDevice sonar = PingDevice(sonarPort);
 
-if (!sonar.initialize(100)){
-    printf("device initialization failed\n");
-} else {
-    printf("pass\n");
-    // printf("Device Type %d id %d hardware revision %d\n", sonar.device_type, sonar.device_id, sonar.device_revision);
-    // printf("Ping Protocol v%d.%d.%d\n", sonar.version_major, sonar.version_minor, sonar.version_patch);
-    // printf("Device Firmware v%d.%d.%d\n", sonar.firmware_version_major, sonar.firmware_version_minor, sonar.firmware_version_patch);
-}
+ if (!sonar.initialize()){
+     printf("device initialization failed\n");
+ } else {
+     //ping_message oi = sonar.read();
+     //std::cout <<  << std::endl;
+     printf("pass\n");
+     printf("Device Type %d id %d hardware revision %d\n", sonar.device_type, sonar.device_id, sonar.device_revision);
+     printf("Ping Protocol v%d.%d.%d\n", sonar.version_major, sonar.version_minor, sonar.version_patch);
+     printf("Device Firmware v%d.%d.%d\n", sonar.firmware_version_major, sonar.firmware_version_minor, sonar.firmware_version_patch);
+ }
 
     return 0;
 }

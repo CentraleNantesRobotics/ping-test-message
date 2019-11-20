5e95c6dbb71f092fffffc87e94be97ab437178a0
Henrique Martinez <rocamora.henrique@gmail.com>
2019-11-20 14:42:08 +0100

    corrected links in cmakelists

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "ping-port-linux.h"
-#include <ping-device-ping360.h>
+#include <ping-device-ping1d.h>
 #include "ping-message-common.h"
-#include "ping-message-ping360.h"
+#include "ping-message-ping1d.h"
 #include "ping-port-linux.h"
 #include <iostream>
 
@@ -9,15 +9,15 @@
 int main(){
     const char* port = "/dev/ttyUSB0";
     PingPortLinux sonarPort = PingPortLinux(port);
-    Ping360 sonar = Ping360(sonarPort);
+    Ping1d sonar = Ping1d(sonarPort);
 
 if (!sonar.initialize(100)){
     printf("device initialization failed\n");
 } else {
     printf("pass\n");
-    printf("Device Type %d id %d hardware revision %d\n", sonar.device_type, sonar.device_id, sonar.device_revision);
-    printf("Ping Protocol v%d.%d.%d\n", sonar.version_major, sonar.version_minor, sonar.version_patch);
-    printf("Device Firmware v%d.%d.%d\n", sonar.firmware_version_major, sonar.firmware_version_minor, sonar.firmware_version_patch);
+    // printf("Device Type %d id %d hardware revision %d\n", sonar.device_type, sonar.device_id, sonar.device_revision);
+    // printf("Ping Protocol v%d.%d.%d\n", sonar.version_major, sonar.version_minor, sonar.version_patch);
+    // printf("Device Firmware v%d.%d.%d\n", sonar.firmware_version_major, sonar.firmware_version_minor, sonar.firmware_version_patch);
 }
 
     return 0;
